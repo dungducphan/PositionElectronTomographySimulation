9d0491bd21b94217a7dbcc78cb49795239c51157
Dung Phan <brianp.dung@gmail.com>
2018-12-03 14:52:19 -0600

    Making replica of single module to make PET ring and PET scan tube.

--- include/PETDetectorConstruction.hh
+++ include/PETDetectorConstruction.hh
@@ -13,7 +13,10 @@
 #include "G4SystemOfUnits.hh"
 #include "G4PhysicalConstants.hh"
 #include "G4VisAttributes.hh"
+#include "G4AssemblyVolume.hh"
 #include "globals.hh"
+
+#include "PETTrackerSD.hh"
 
 class PETDetectorMessenger;
 
@@ -23,54 +26,73 @@ class G4LogicalVolume;
 
 class PETDetectorConstruction : public G4VUserDetectorConstruction {
 public:
-  PETDetectorConstruction();
+    PETDetectorConstruction();
 
-  virtual ~PETDetectorConstruction();
+    virtual ~PETDetectorConstruction();
 
-  virtual G4VPhysicalVolume *Construct();
+    virtual G4VPhysicalVolume *Construct();
 
-  virtual void BuildMaterial();
+    virtual void BuildMaterial();
 
-  virtual void ConstructSDandField();
+    virtual void ConstructSDandField();
 
-  // set methods
-  void SetOpeningAngle(G4double newValue);
+    // set methods
+    void SetOpeningAngle(G4double newValue);
 
-  void SetInnerDiameter(G4double newValue);
+    void SetInnerDiameter(G4double newValue);
 
-  void SetOuterDiameter(G4double newValue);
+    void SetOuterDiameter(G4double newValue);
 
-  void SetHeight(G4double newValue);
+    void SetHeight(G4double newValue);
+
+    // Geometry Calculation methods
+    G4int CalculateNReplicaInARing();
 
 private:
-  G4double fOpeningAngle;
-  G4double fInnerDiameter;
-  G4double fOuterDiameter;
-  G4double fHeight;
+    // Single Crystal Variables
+    G4double fOpeningAngle;
+    G4double fInnerDiameter;
+    G4double fOuterDiameter;
+    G4double fHeight;
 
-  G4double world_hx;
-  G4double world_hy;
-  G4double world_hz;
-  G4Box *worldBox;
-  G4LogicalVolume *worldLog;
-  G4VPhysicalVolume *worldPhy;
+    G4Tubs *sCrystal_Mod;
+    G4Tubs *sDet_Mod;
+    G4Tubs *sCrystalDet_Mod;
+    G4LogicalVolume *sCrystal_Log;
+    G4LogicalVolume *sDet_Log;
+    G4LogicalVolume *sCrystalDet_Log;
+//    G4VPhysicalVolume *sCrystal_Phy;
+//    G4VPhysicalVolume *sDet_Phy;
+//    G4VPhysicalVolume *sCrystalDet_Phy;
 
-  G4Tubs *sCrystal_Mod;
-  G4Tubs *sDet_Mod;
-  G4LogicalVolume *sCrystal_Log;
-  G4LogicalVolume *detectorLog;
-  G4VPhysicalVolume *sCrystal_Phy;
-  G4VPhysicalVolume *detectorPhy;
+    PETTrackerSD *aTrackerSD;
 
-  G4Material *fAir;
-  G4Material *fLYSO;
-  G4Material *fLutetiumOxide;
-  G4Material *fYttriumOxide;
-  G4Material *fSiliconDioxide;
-  G4MaterialPropertiesTable *fLYSO_MPT;
-  G4MaterialPropertiesTable *fAir_MPT;
+    // Ring of Crystal Modules Variables
+    G4double fRingInnerDiameterBuffer;
+    G4double fRingOuterDiameterBuffer;
+    G4double fRingThicknessBuffer;
 
-  PETDetectorMessenger *fMessenger;
+    G4Tubs            *crystalRing_Mod;
+    G4LogicalVolume   *crystalRing_Log;
+//    G4VPhysicalVolume *crystalRing_Phy;
+
+    // General Variables
+    G4double world_hx;
+    G4double world_hy;
+    G4double world_hz;
+    G4Box *worldBox;
+    G4LogicalVolume *worldLog;
+    G4VPhysicalVolume *worldPhy;
+
+    G4Material *fAir;
+    G4Material *fLYSO;
+    G4Material *fLutetiumOxide;
+    G4Material *fYttriumOxide;
+    G4Material *fSiliconDioxide;
+    G4MaterialPropertiesTable *fLYSO_MPT;
+    G4MaterialPropertiesTable *fAir_MPT;
+
+    PETDetectorMessenger *fMessenger;
 };
 
 #endif
--- src/PETDetectorConstruction.cc
+++ src/PETDetectorConstruction.cc
@@ -1,6 +1,5 @@
 #include "PETDetectorConstruction.hh"
 #include "PETDetectorMessenger.hh"
-#include "PETTrackerSD.hh"
 
 #include "G4SDManager.hh"
 #include "G4PhysicalConstants.hh"
@@ -12,26 +11,30 @@
 #include "G4PhysicalVolumeStore.hh"
 #include "G4LogicalVolumeStore.hh"
 #include "G4SolidStore.hh"
+#include "G4PVReplica.hh"
+
+#include "TMath.h"
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 
+
 PETDetectorConstruction::PETDetectorConstruction()
-: G4VUserDetectorConstruction(),
-  world_hx(5.0 * m),
-  world_hy(5.0 * m),
-  world_hz(5.0 * m),
-  fMessenger(NULL),
-  fLYSO_MPT(NULL),
-  fAir_MPT(NULL),
-  fOpeningAngle(1.5),
-  fInnerDiameter(50*cm),
-  fOuterDiameter(60*cm),
-  fHeight(10*cm),
-  worldLog(NULL),
-  worldPhy(NULL),
-  sCrystal_Log(NULL) {
+        : G4VUserDetectorConstruction(),
+          world_hx(1.0 * m),
+          world_hy(1.0 * m),
+          world_hz(1.0 * m),
+          fMessenger(NULL),
+          fLYSO_MPT(NULL),
+          fAir_MPT(NULL),
+          fOpeningAngle(1.5),
+          fInnerDiameter(50 * cm),
+          fOuterDiameter(60 * cm),
+          fHeight(10 * cm),
+          worldLog(NULL),
+          worldPhy(NULL),
+          sCrystal_Log(NULL) {
     fMessenger = new PETDetectorMessenger(this);
 }
 
@@ -39,161 +42,183 @@ PETDetectorConstruction::~PETDetectorConstruction() {
 }
 
 void PETDetectorConstruction::BuildMaterial() {
-  G4NistManager * nist = G4NistManager::Instance();
+    G4NistManager *nist = G4NistManager::Instance();
 
-  G4Element *Oxigen = nist->FindOrBuildElement("O");
-  G4Element *Lutetium = nist->FindOrBuildElement("Lu");
-  G4Element *Yttrium = nist->FindOrBuildElement("Y");
+    G4Element *Oxigen = nist->FindOrBuildElement("O");
+    G4Element *Lutetium = nist->FindOrBuildElement("Lu");
+    G4Element *Yttrium = nist->FindOrBuildElement("Y");
 
-  // Air
-  fAir = nist->FindOrBuildMaterial("G4_AIR");
+    // Air
+    fAir = nist->FindOrBuildMaterial("G4_AIR");
 
-  // Lu2O3
-  fLutetiumOxide = new G4Material("LutetiumOxide", 9.41 * g / cm3, 2);
-  fLutetiumOxide->AddElement(Lutetium, 2);
-  fLutetiumOxide->AddElement(Oxigen, 3);
+    // Lu2O3
+    fLutetiumOxide = new G4Material("LutetiumOxide", 9.41 * g / cm3, 2);
+    fLutetiumOxide->AddElement(Lutetium, 2);
+    fLutetiumOxide->AddElement(Oxigen, 3);
 
-  // SiO2
-  fSiliconDioxide = nist->FindOrBuildMaterial("G4_SILICON_DIOXIDE");
+    // SiO2
+    fSiliconDioxide = nist->FindOrBuildMaterial("G4_SILICON_DIOXIDE");
 
-  // Y2O3
-  fYttriumOxide = new G4Material("YttriumOxide", 5.01 * g / cm3, 2);
-  fYttriumOxide->AddElement(Yttrium, 2);
-  fYttriumOxide->AddElement(Oxigen, 3);
+    // Y2O3
+    fYttriumOxide = new G4Material("YttriumOxide", 5.01 * g / cm3, 2);
+    fYttriumOxide->AddElement(Yttrium, 2);
+    fYttriumOxide->AddElement(Oxigen, 3);
 
-  // LYSO
-  fLYSO = new G4Material("LYSO", 7.1 * g / cm3, 3);
-  fLYSO->AddMaterial(fLutetiumOxide, 81 * perCent);
-  fLYSO->AddMaterial(fSiliconDioxide, 14 * perCent);
-  fLYSO->AddMaterial(fYttriumOxide, 5 * perCent);
+    // LYSO
+    fLYSO = new G4Material("LYSO", 7.1 * g / cm3, 3);
+    fLYSO->AddMaterial(fLutetiumOxide, 81 * perCent);
+    fLYSO->AddMaterial(fSiliconDioxide, 14 * perCent);
+    fLYSO->AddMaterial(fYttriumOxide, 5 * perCent);
 
-  // Optical Properties of Materials
-  const G4int num = 21;
-  G4double ene[num] = {1.79 * eV, 1.85 * eV, 1.91 * eV, 1.97 * eV,
-                       2.04 * eV, 2.11 * eV, 2.19 * eV, 2.27 * eV,
-                       2.36 * eV, 2.45 * eV, 2.56 * eV, 2.67 * eV,
-                       2.80 * eV, 2.94 * eV, 3.09 * eV, 3.25 * eV,
-                       3.44 * eV, 3.65 * eV, 3.89 * eV, 4.16 * eV,
-                       4.43 * eV};
+    // Optical Properties of Materials
+    const G4int num = 21;
+    G4double ene[num] = {1.79 * eV, 1.85 * eV, 1.91 * eV, 1.97 * eV,
+                         2.04 * eV, 2.11 * eV, 2.19 * eV, 2.27 * eV,
+                         2.36 * eV, 2.45 * eV, 2.56 * eV, 2.67 * eV,
+                         2.80 * eV, 2.94 * eV, 3.09 * eV, 3.25 * eV,
+                         3.44 * eV, 3.65 * eV, 3.89 * eV, 4.16 * eV,
+                         4.43 * eV};
 
-  G4double fast[num] = {0.01, 0.10, 0.20, 0.50,
-                        0.90, 1.70, 2.90, 5.00,
-                        8.30, 12.5, 17.0, 22.9,
-                        26.4, 25.6, 16.8, 4.20,
-                        0.30, 0.20, 0.10, 0.01,
-                        0.00};
+    G4double fast[num] = {0.01, 0.10, 0.20, 0.50,
+                          0.90, 1.70, 2.90, 5.00,
+                          8.30, 12.5, 17.0, 22.9,
+                          26.4, 25.6, 16.8, 4.20,
+                          0.30, 0.20, 0.10, 0.01,
+                          0.00};
 
-  G4double rLyso[num] = {1.81, 1.81, 1.81, 1.81,
-                         1.81, 1.81, 1.81, 1.81,
-                         1.81, 1.81, 1.81, 1.81,
-                         1.81, 1.81, 1.81, 1.81,
-                         1.81, 1.81, 1.81, 1.81,
-                         1.81};
-  G4double rAir[num] = {1.00, 1.00, 1.00, 1.00,
-                        1.00, 1.00, 1.00, 1.00,
-                        1.00, 1.00, 1.00, 1.00,
-                        1.00, 1.00, 1.00, 1.00,
-                        1.00, 1.00, 1.00, 1.00,
-                        1.00};
+    G4double rLyso[num] = {1.81, 1.81, 1.81, 1.81,
+                           1.81, 1.81, 1.81, 1.81,
+                           1.81, 1.81, 1.81, 1.81,
+                           1.81, 1.81, 1.81, 1.81,
+                           1.81, 1.81, 1.81, 1.81,
+                           1.81};
+    G4double rAir[num] = {1.00, 1.00, 1.00, 1.00,
+                          1.00, 1.00, 1.00, 1.00,
+                          1.00, 1.00, 1.00, 1.00,
+                          1.00, 1.00, 1.00, 1.00,
+                          1.00, 1.00, 1.00, 1.00,
+                          1.00};
 
-  G4double abs[num] = {50 * cm, 50 * cm, 50 * cm, 50 * cm,
-                       50 * cm, 50 * cm, 50 * cm, 50 * cm,
-                       50 * cm, 50 * cm, 50 * cm, 50 * cm,
-                       50 * cm, 50 * cm, 50 * cm, 50 * cm,
-                       50 * cm, 50 * cm, 50 * cm, 50 * cm,
-                       50 * cm};
+    G4double abs[num] = {50 * cm, 50 * cm, 50 * cm, 50 * cm,
+                         50 * cm, 50 * cm, 50 * cm, 50 * cm,
+                         50 * cm, 50 * cm, 50 * cm, 50 * cm,
+                         50 * cm, 50 * cm, 50 * cm, 50 * cm,
+                         50 * cm, 50 * cm, 50 * cm, 50 * cm,
+                         50 * cm};
 
-  fLYSO_MPT = new G4MaterialPropertiesTable();
-  fAir_MPT = new G4MaterialPropertiesTable();
+    fLYSO_MPT = new G4MaterialPropertiesTable();
+    fAir_MPT = new G4MaterialPropertiesTable();
 
-  fLYSO_MPT->AddProperty("FASTCOMPONENT", ene, fast, num);
-  fLYSO_MPT->AddProperty("RINDEX", ene, rLyso, num);
-  fLYSO_MPT->AddProperty("ABSLENGTH", ene, abs, num);
-  fLYSO_MPT->AddConstProperty("SCINTILLATIONYIELD", 0.5 / keV);
-  fLYSO_MPT->AddConstProperty("RESOLUTIONSCALE", 1);
-  fLYSO_MPT->AddConstProperty("FASTTIMECONSTANT", 43 * ns);
+    fLYSO_MPT->AddProperty("FASTCOMPONENT", ene, fast, num);
+    fLYSO_MPT->AddProperty("RINDEX", ene, rLyso, num);
+    fLYSO_MPT->AddProperty("ABSLENGTH", ene, abs, num);
+    fLYSO_MPT->AddConstProperty("SCINTILLATIONYIELD", 0.5 / keV);
+    fLYSO_MPT->AddConstProperty("RESOLUTIONSCALE", 1);
+    fLYSO_MPT->AddConstProperty("FASTTIMECONSTANT", 43 * ns);
 
-  fAir_MPT->AddProperty("RINDEX", ene, rAir, num);
+    fAir_MPT->AddProperty("RINDEX", ene, rAir, num);
 
-  fLYSO->SetMaterialPropertiesTable(fLYSO_MPT);
-  fAir->SetMaterialPropertiesTable(fAir_MPT);
+    fLYSO->SetMaterialPropertiesTable(fLYSO_MPT);
+    fAir->SetMaterialPropertiesTable(fAir_MPT);
 }
 
-G4VPhysicalVolume * PETDetectorConstruction::Construct() {
-  BuildMaterial();
-  
-  // Overlapping check
-  G4bool checkOverlaps = true;
+G4VPhysicalVolume *PETDetectorConstruction::Construct() {
+    BuildMaterial();
 
-  // Clear old geometry if any
-  if (worldPhy) {
-      G4GeometryManager::GetInstance()->OpenGeometry();
-      G4PhysicalVolumeStore::GetInstance()->Clean();
-      G4LogicalVolumeStore::GetInstance()->Clean();
-      G4SolidStore::GetInstance()->Clean();
-  }
+    // Overlapping check
+    G4bool checkOverlaps = true;
 
-  sCrystal_Mod = new G4Tubs(
-    "SingleCrystalModel",
-    fInnerDiameter,
-    fOuterDiameter,
-    fHeight*0.5,
-    0,
-    fOpeningAngle);
+    // Clear old geometry if any
+    if (worldPhy) {
+        G4GeometryManager::GetInstance()->OpenGeometry();
+        G4PhysicalVolumeStore::GetInstance()->Clean();
+        G4LogicalVolumeStore::GetInstance()->Clean();
+        G4SolidStore::GetInstance()->Clean();
+    }
 
-  sDet_Mod = new G4Tubs(
-    "detector",
-    fOuterDiameter,
-    fOuterDiameter+0.2,
-    fHeight*0.5,
-    0,
-    fOpeningAngle);
-  
+    sCrystal_Mod = new G4Tubs(
+            "SingleCrystalModel",
+            fInnerDiameter,
+            fOuterDiameter,
+            fHeight * 0.5,
+            0,
+            fOpeningAngle);
+
+    sDet_Mod = new G4Tubs(
+            "SiPMModel",
+            fOuterDiameter,
+            fOuterDiameter + 0.2,
+            fHeight * 0.5,
+            0,
+            fOpeningAngle);
+
+    sCrystalDet_Mod = new G4Tubs(
+            "SingleCrystalWithSiPMModel",
+            fInnerDiameter,
+            fOuterDiameter + 0.2,
+            fHeight * 0.5,
+            0,
+            fOpeningAngle);
+
+    crystalRing_Mod = new G4Tubs(
+            "SingleCrystalModel",
+            fInnerDiameter - fRingInnerDiameterBuffer,
+            fOuterDiameter + fRingOuterDiameterBuffer,
+            (fHeight + fRingThicknessBuffer) * 0.5,
+            0,
+            2 * TMath::Pi());
+
+    worldBox = new G4Box("WorldBox", world_hx, world_hy, world_hz);
+
+    sDet_Log = new G4LogicalVolume(sDet_Mod, fLYSO, "DetectorLogicalVolume", 0, 0, 0);
+    sCrystal_Log = new G4LogicalVolume(sCrystal_Mod, fLYSO, "SingleCrystalLogicalVolume", 0, 0, 0);
+    sCrystalDet_Log = new G4LogicalVolume(sCrystalDet_Mod, fAir, "SingleCrystalWithSiPMLogicalVolume", 0, 0, 0);
+    crystalRing_Log = new G4LogicalVolume(crystalRing_Mod, fAir, "CrystalRingLogicalVolume", 0, 0, 0);
+    worldLog = new G4LogicalVolume(worldBox, fAir, "WorldLog");
+
+    // Placement
+    new G4PVPlacement(0, G4ThreeVector(), sCrystal_Log, "", sCrystalDet_Log, false, 0);
+    new G4PVPlacement(0, G4ThreeVector(), sDet_Log, "", sCrystalDet_Log, false, 0);
+
+    // Module assembly
+    G4int nCrystalInARing = TMath::Floor((2*TMath::Pi())/fOpeningAngle);
+    G4double widthOfSingleRep = (2*TMath::Pi())/(G4double)nCrystalInARing;
+    new G4PVReplica("Ring", sCrystalDet_Log, crystalRing_Log, kPhi, nCrystalInARing, widthOfSingleRep, 0);
+
+    /* TODO
+     *  - Give the right geometry.
+     *  - But in runtime, the program crashes. My guess is that I need to
+     *  rewrite the TrackerSD. Also, change name of class TrackSD. SiPM is not a tracker.
+     */
+    new G4PVReplica("Tube", crystalRing_Log, worldLog, kZAxis, 10, fHeight + 1*mm, 0);
+
+    // Return physical volumes
+    worldPhy = new G4PVPlacement(0, G4ThreeVector(), worldLog, "WorldPhy", 0, false, 0, checkOverlaps);
 
 
+    // Vis touch-up
+    G4VisAttributes *crystVisAttributes = new G4VisAttributes(G4Colour(0.0, 0.0, 1.0));
+    G4VisAttributes *SiPMVisAttributes = new G4VisAttributes(G4Colour(1.0, 0.0, 0.0));
+    sCrystal_Log->SetVisAttributes(crystVisAttributes);
+    sDet_Log->SetVisAttributes(SiPMVisAttributes);
 
-  detectorLog = new G4LogicalVolume(sDet_Mod, fLYSO, "DetectorLog", 0, 0, 0);
-    
-			
-			
-  sCrystal_Log = new G4LogicalVolume(sCrystal_Mod, fLYSO, "SingleCrystalLogicalVolume", 0, 0, 0);
+    // Surface properties of the detector
+    G4OpticalSurface *opDetectorSurface = new G4OpticalSurface("DetectorSurface");
+    opDetectorSurface->SetType(dielectric_metal);
+    opDetectorSurface->SetFinish(ground);
+    opDetectorSurface->SetModel(glisur);
+    G4LogicalSkinSurface *detectorSurface = new G4LogicalSkinSurface("DetectorSurface", sDet_Log, opDetectorSurface);
 
-  worldBox = new G4Box("WorldBox", world_hx, world_hy, world_hz);
-  worldLog = new G4LogicalVolume(worldBox, fAir, "WorldLog");
+    G4double ephoton_pmt[4] = {0.0001 * eV, 1 * eV, 10 * eV, 100 * eV};
+    G4double reflectivity_pmt[4] = {0, 0, 0, 0};
+    G4double efficiency_pmt[4] = {1.0, 1.0, 1.0, 1.0};
 
+    G4MaterialPropertiesTable *detectorMPT = new G4MaterialPropertiesTable();
+    detectorMPT->AddProperty("REFLECTIVITY", ephoton_pmt, reflectivity_pmt, 4);
+    detectorMPT->AddProperty("EFFICIENCY", ephoton_pmt, efficiency_pmt, 4);
+    opDetectorSurface->SetMaterialPropertiesTable(detectorMPT);
 
-  sCrystal_Phy = new G4PVPlacement(0, G4ThreeVector(), sCrystal_Log, "SingleCrystalPhysicalVolume", worldLog, false, 0);
-  detectorPhy = new G4PVPlacement(0, G4ThreeVector(), detectorLog, "DetectorPhy", worldLog, false, 0);
-  worldPhy = new G4PVPlacement(0, G4ThreeVector(), worldLog, "WorldPhy", 0, false, 0, checkOverlaps);
-
-  G4VisAttributes* crystVisAttributes = new G4VisAttributes(G4Colour(1.0,1.0,1.0));
-  G4VisAttributes* SiPMVisAttributes = new G4VisAttributes(G4Colour(0.0,1.0,0.0));
-
-  sCrystal_Log->SetVisAttributes(crystVisAttributes);
-  detectorLog->SetVisAttributes(SiPMVisAttributes);
-
-
-
-  G4OpticalSurface* opDetectorSurface = new G4OpticalSurface("DetectorSurface");
-  opDetectorSurface->SetType(dielectric_metal);
-  opDetectorSurface->SetFinish(ground);
-  opDetectorSurface->SetModel(glisur);
-  G4LogicalSkinSurface* detectorSurface = new G4LogicalSkinSurface("DetectorSurface", detectorLog, opDetectorSurface);
-
-
-  G4double ephoton_pmt[4]      = {0.0001*eV, 1*eV, 10*eV, 100*eV};
-  G4double reflectivity_pmt[4] = {0,0,0,0};
-  G4double efficiency_pmt[4]   = {1.0,1.0,1.0,1.0};
-
-  G4MaterialPropertiesTable *detectorMPT = new G4MaterialPropertiesTable();
-  detectorMPT->AddProperty("REFLECTIVITY", ephoton_pmt, reflectivity_pmt, 4);
-  detectorMPT->AddProperty("EFFICIENCY",   ephoton_pmt, efficiency_pmt,   4);
-  opDetectorSurface->SetMaterialPropertiesTable(detectorMPT);
-
-  
-
-
-  return worldPhy;
+    return worldPhy;
 }
 
 void PETDetectorConstruction::SetOpeningAngle(G4double newValue) {
@@ -216,15 +241,17 @@ void PETDetectorConstruction::SetHeight(G4double newValue) {
     G4RunManager::GetRunManager()->DefineWorldVolume(Construct());
 }
 
-
-
 void PETDetectorConstruction::ConstructSDandField() {
-  // Sensitive detectors
+    // Sensitive detectors
 
-  PETTrackerSD* aTrackerSD = new PETTrackerSD("TrackerChamberSD");
-  G4SDManager::GetSDMpointer()->AddNewDetector(aTrackerSD);
+    aTrackerSD = new PETTrackerSD("TrackerChamberSD");
+    G4SDManager::GetSDMpointer()->AddNewDetector(aTrackerSD);
 
-  // Setting aTrackerSD to all logical volumes with the same name of "Chamber_LV".
-  SetSensitiveDetector("DetectorLog", aTrackerSD, true);
+    // Setting aTrackerSD to all logical volumes with the same name of "Chamber_LV".
+    SetSensitiveDetector("DetectorLogicalVolume", aTrackerSD, true);
+}
+
+G4int PETDetectorConstruction::CalculateNReplicaInARing() {
+    return TMath::Floor((2 * TMath::Pi()) / this->fOpeningAngle);
 }
 
