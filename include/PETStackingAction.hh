63b3fe0397208f864d1bdb4c43cfa50e9c3d6f26
biafox <41394727+biafox@users.noreply.github.com>
2019-03-25 15:45:36 -0500

    Adding Stacking Action

---
+++ include/PETStackingAction.hh
@@ -0,0 +1,29 @@
+#ifndef PETStackingaction_H
+#define PETStackingAction_H 1
+
+#include "G4UserStackingAction.hh"
+#include "G4RunManager.hh"
+#include "PETRunAction.hh"
+
+
+class PETRunAction;
+
+class PETStackingAction : public G4UserStackingAction
+{
+  public:
+    PETStackingAction(PETRunAction *runAction);
+    virtual ~PETStackingAction();
+
+  public:
+    virtual G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track* aTrack);
+    virtual void NewStage();
+    virtual void PrepareNewEvent();
+
+  private:
+  PETRunAction *fRunAction;
+  
+  G4int gammaCounter;
+  
+};
+
+#endif
